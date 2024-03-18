777cd2fd5fae45ace627ba0c41173976c159d7f7
Prajeet <prajeet911@gmail.com>
2024-03-18 17:03:46 +0545

    Fix: Styles

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -1,34 +1,64 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { trpc } from "../_trpc/client";
 
 export default function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
-  const addTodo = trpc.addTodo.useMutation();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
 
   const [content, setContent] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const todosResponse = await fetch("/api/trpc/getTodos");
+      const todos = await todosResponse.json();
+    }
+    getData();
+  });
+
   return (
-    <div>
-      <div>{JSON.stringify(getTodos.data)}</div>
+    <div className="flex flex-col gap-3">
+      <h1 className="text-3xl">Todos</h1>
       <div>
-        <label htmlFor="content">Content: </label>
-        <input
-          id="content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="text-black"
-        />
-        <button
-          onClick={async () => {
-            if (content.length) {
-              addTodo.mutate(content);
-              setContent("");
-            }
-          }}
-        >
-          Add todo
-        </button>
+        {getTodos?.data?.map((todo) => (
+          <div key={todo.id} className="flex gap-3 items-center">
+            <input
+              id={`check-${todo.id}`}
+              type="checkbox"
+              checked={!!todo.done}
+              style={{ zoom: 1.5 }}
+            />
+            <label htmlFor={`check-${todo.id}`}>{todo.content}</label>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div className="text-2xl mb-2 mt-4">Add a todo</div>
+        <div className="flex gap-3 items-center">
+          <label htmlFor="content">Content: </label>
+          <input
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="text-black border-2 h-[40px]"
+          />
+          <button
+            onClick={async () => {
+              if (content.length) {
+                addTodo.mutate(content);
+                setContent("");
+              }
+            }}
+            className="bg-blue-500 text-white p-2 rounded-lg"
+          >
+            Add todo
+          </button>
+        </div>
       </div>
     </div>
   );
